7d25c29a576f9cb522201ab4e209d9f85c4f9cb8
Gabriël Konat <gabrielkonat@gmail.com>
2013-02-26 15:29:00 +0100

    Spoofax editor for Java.

---
+++ editor/java/java/JavaValidator.java
@@ -0,0 +1,12 @@
+package java;
+
+import org.strategoxt.imp.runtime.dynamicloading.Descriptor;
+import org.strategoxt.imp.runtime.services.MetaFileLanguageValidator;
+
+public class JavaValidator extends MetaFileLanguageValidator 
+{ 
+  @Override public Descriptor getDescriptor()
+  {
+    return JavaParseController.getDescriptor();
+  }
+}
