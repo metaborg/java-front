03af811766405d4a5c3aae911d2afa4e3a9c61f7
Eduardo Souza <eduardo7.ufv@gmail.com>
2017-02-23 11:08:25 +0100

    Java 1.8 syntax

---
+++ lang.java/src/main/strategies/lang/java/strategies/InteropRegisterer.java
@@ -0,0 +1,10 @@
+package lang.java.strategies;
+
+import org.strategoxt.lang.JavaInteropRegisterer;
+import org.strategoxt.lang.Strategy;
+
+public class InteropRegisterer extends JavaInteropRegisterer {
+    public InteropRegisterer() {
+        super(new Strategy[] { });
+    }
+}
