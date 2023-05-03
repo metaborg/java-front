3edc9ee9e881a5e1a9c06e5bf5d7b8f551a9408c
Jeff Smits <mail@jeffsmits.net>
2023-05-03 10:00:42 +0200

    Update version script for releases to not do snapshots, those aren't used for releases

--- set_release_version.sh
+++ set_release_version.sh
@@ -3,24 +3,18 @@
 set -eu
 
 if [ "$#" -eq "0" ]; then
-  echo "USAGE:\n    $0 VERSION SPOOFAX_VERSION\n    $0 OLD_VERSION NEW_VERSION SPOOFAX_VERSION\n\nThe first option assumes you are on the given version and just want to strip off the -SNAPSHOT. \n\nReplaces \${OLD_VERSION}-SNAPSHOT with \$NEW_VERSION, updates Spoofax to latest release version. \n\$SPOOFAX_VERSION should also be this latest release version to update a file with that cannot be auto-updated with maven. "
+  echo "USAGE:\n    $0 OLD_VERSION NEW_VERSION SPOOFAX_VERSION\n\nReplaces \${OLD_VERSION} with \$NEW_VERSION, updates Spoofax to latest release version. \n\$SPOOFAX_VERSION should also be this latest release version to update a file with that cannot be auto-updated with maven. "
   exit 0
 fi
 
-if [ "$#" -eq "3" ]; then
-  OLD_VERSION=$1
-  NEW_VERSION=$2
-  SPOOFAX_VERSION=$3
-else
-  OLD_VERSION=$1
-  NEW_VERSION=$1
-  SPOOFAX_VERSION=$2
-fi
+OLD_VERSION=$1
+NEW_VERSION=$2
+SPOOFAX_VERSION=$3
 
 PROJECTS="lang.java lang.java.test lang.java.javac lang.java.statics lang.java.statics.test lang.java.example"
 
 for project in $PROJECTS; do
-  sed -i'' -e "s/$OLD_VERSION-SNAPSHOT/$NEW_VERSION/g" $project/metaborg.yaml
+  sed -i'' -e "s/$OLD_VERSION/$NEW_VERSION/g" $project/metaborg.yaml
 done
 
 for project in . $PROJECTS; do
@@ -28,9 +22,9 @@ for project in . $PROJECTS; do
 done
 sed -i'' -e "s|<metaborg-version>.*</metaborg-version>|<metaborg-version>$SPOOFAX_VERSION</metaborg-version>|g" jar-analyzer/pom.xml
 
-sed -i'' -e "s/$OLD_VERSION.qualifier/$NEW_VERSION/g" lang.java.eclipse.site/site.xml
-sed -i'' -e "s/$OLD_VERSION.qualifier/$NEW_VERSION/g" lang.java.eclipse.feature/feature.xml
-sed -i'' -e "s/$OLD_VERSION.qualifier/$NEW_VERSION/g" lang.java.eclipse/META-INF/MANIFEST.MF
+sed -i'' -e "s/$OLD_VERSION/$NEW_VERSION/g" lang.java.eclipse.site/site.xml
+sed -i'' -e "s/$OLD_VERSION/$NEW_VERSION/g" lang.java.eclipse.feature/feature.xml
+sed -i'' -e "s/$OLD_VERSION/$NEW_VERSION/g" lang.java.eclipse/META-INF/MANIFEST.MF
 
 # Update Spoofax to newest released version
 for project in . $PROJECTS; do
